e90844c13cf6fa9392f76dbfc7723848282571c3
Abbas Sher <57453785+Abbassher55@users.noreply.github.com>
2024-07-31 00:19:16 +0500

    new styles inserted

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["./dist/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "rose-50": "#FCF8F6",
+        "rose-100": "#F5EEEC",
+        "rose-300": "#CAAFA7",
+        "rose-400": "#AD8A85",
+        "rose-500": "#87635A",
+        "rose-900": "#260F08",
+        red: "#C73B0F",
+        green: "#1EA575",
+      },
+    },
   },
   plugins: [],
 };
